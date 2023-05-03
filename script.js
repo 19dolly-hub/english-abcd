f24f8494bf5fefb8f639e348b413a017cbf2bce7
19dolly-hub <132393903+19dolly-hub@users.noreply.github.com>
2023-05-03 11:40:09 +0530

    added all files

---
+++ script.js
@@ -0,0 +1,38 @@
+
+// adding bg to navbar on scroll
+
+let nav = document.querySelector('.navbar');
+
+function addBg() {
+  console.log('scroll detected');
+  nav.classList.add('nav-bg');
+} 
+
+let el = document;
+
+if (el) {
+  console.log('document found');
+  el.addEventListener('scroll', addBg);
+} else {
+  console.error('Document element not found');
+}
+
+// changing navbar style according to screen size
+
+let rightNav = document.querySelector('.right-nav');
+
+function sideNav() {
+  console.log('click detected');
+  rightNav.classList.toggle('opacity');
+}
+
+// activating side nav on click
+
+let btn = document.querySelector('.nav-btn');
+
+if (btn) {
+  console.log('button found');
+  btn.addEventListener('click', sideNav);
+} else {
+  console.error('button not found');
+}
